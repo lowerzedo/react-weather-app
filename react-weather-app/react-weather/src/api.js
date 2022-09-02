8ad12a24b74683d4e16f8754f7a5b7263961f72e
Nazar <61383270+lowerzedo@users.noreply.github.com>
2022-09-02 14:01:17 +0800

    Add files via upload

---
+++ react-weather-app/react-weather/src/api.js
@@ -0,0 +1,12 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'bfd4df5c43msh35ddf9b87285a06p1adc09jsn3eb0e34f4685',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo"
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5/"
+export const WEATHER_API_KEY = "22851d7abc64f860cdc9489da0048fc3"
